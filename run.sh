ba266a0424ab1ea499e37dc82b3785dee93a5e39
dydjw9 <dydjw9@gmail.com>
2022-05-16 11:57:10 +0800

    first

---
+++ run.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 3epochago.py /home/djw/data/imagenet --model vit_small_patch32_224 -b 256 --lr 3e-3 --sched cosine --epochs 300  --model-ema --opt adamw -j 8 --warmup-lr 1e-6 --weight-decay 0.3 --drop 0.0 --drop-path .1 --warmup-epochs 5 --img-size 224   --smoothing 0.1 --native-amp
